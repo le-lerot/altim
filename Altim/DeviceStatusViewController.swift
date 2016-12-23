220a289d0395d070f7caccc3aa08789a46ea1282
cocodelille <corentindelille@MacBook-Pro-de-Corentin.local>
2016-12-23 16:40:06 +0100

    Connecting yaw pitch roll label to device motion sensor using CoreMotion

--- Altim/DeviceStatusViewController.swift
+++ Altim/DeviceStatusViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import CoreMotion
 
 class DeviceStatusViewController: UIViewController {
 
+    //MARK: parameters
+    @IBOutlet weak var yaw: UILabel!
+    @IBOutlet weak var pitch: UILabel!
+    @IBOutlet weak var roll: UILabel!
+    
+    var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // init MotionManager
+        motionManager.deviceMotionUpdateInterval = 0.1
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (deviceMotion, error) in
+            self.yaw.text = String(format: "%f%", (deviceMotion?.attitude.yaw)!)
+            self.pitch.text = String(format: "%f%", (deviceMotion?.attitude.pitch)!)
+            self.roll.text = String(format: "%f%", (deviceMotion?.attitude.roll)!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
