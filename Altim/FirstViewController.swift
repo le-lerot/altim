121aceb7ef9785ddb1a1319de80adb00a79e4b14
Martin Delille <martin@phonations.com>
2016-12-23 14:31:59 +0100

    commiting empty project

---
+++ Altim/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Altim
+//
+//  Created by Martin Delille on 23/12/2016.
+//  Copyright © 2016 Phonations. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
